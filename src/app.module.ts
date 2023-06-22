c1dcdc63f3ad5c8181f6bbaad779b6a1afd1012a
ldcss <ldcs@cin.ufpe.br>
2023-06-21 22:17:27 -0300

    <build> set config to run the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: Node.post
-      host
-    })
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppDataSource } from './app.datasource';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/todo/api');
+
+  await AppDataSource.initialize()
+    .then(() => console.log('LOG [Typeorm] Success connection'))
+    .catch(error => console.log(error));
+
+  app.enableCors({
+    origin: 'https://localhost:3001',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
