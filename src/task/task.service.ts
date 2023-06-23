5625f92b969281656619b836b04222cc19425b9e
ldcss <ldcs@cin.ufpe.br>
2023-06-23 01:55:21 -0300

    <feat> implement find all and create functions

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from './dto/createTask.dto';
+import { Task } from './entities/task.entity';
+import { AppDataSource } from 'src/app.datasource';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  async create(createTaskDto: CreateTaskDto) {
+    console.log('to no create');
+    const task = new Task();
+    task.name = createTaskDto.name;
+    task.description = createTaskDto.description;
+    task.priority = createTaskDto.priority;
+    task.finished = false;
+
+    return await AppDataSource.createQueryBuilder().insert().into(Task).values(task).execute();
+  }
+
+  async findAll(): Promise<Task[]> {
+    const tasks = await AppDataSource.createQueryBuilder().select('t').from(Task, 't').getMany();
+    return tasks;
+  }
+}
