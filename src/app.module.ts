4a58e1e73b092c1f145afebc418d9860b2cc43d6
ldcss <ldcs@cin.ufpe.br>
2023-06-23 01:54:52 -0300

    <build> config to mysql connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
-import { TaskModule } from './task/task.module';
 import { MemberModule } from './member/member.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from './app.datasource';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, TaskModule, MemberModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(AppDataSource.options),
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
