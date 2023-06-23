68323f3cb7ac5cd6ea50704bc3643dc28a9062c5
ldcss <ldcs@cin.ufpe.br>
2023-06-23 01:58:03 -0300

    <feat> implement find all members and fix bug in member module,

--- src/member/member.module.ts
+++ src/member/member.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { MemberController } from './member.controller';
 import { MemberService } from './member.service';
 import { TaskModule } from 'src/task/task.module';
-import { UserModule } from 'src/user/user.module';
 
 @Module({
   controllers: [MemberController],
   providers: [MemberService],
-  imports: [TaskModule, UserModule],
+  imports: [TaskModule],
 })
 export class MemberModule {}
--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { AppDataSource } from 'src/app.datasource';
+import { Member } from './entities/member.entity';
 
 @Injectable()
-export class MemberService {}
+export class MemberService {
+  async findAll(): Promise<Member[]> {
+    const members = await AppDataSource.createQueryBuilder()
+      .select('m')
+      .from(Member, 'm')
+      .getMany();
+    return members;
+  }
+}
