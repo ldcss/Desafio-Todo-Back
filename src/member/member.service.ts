c9ae4a0dc2d502e2d16c905577344a44e0a4ddc5
ldcss <ldcs@cin.ufpe.br>
2023-06-23 13:37:34 -0300

    <feat> add method to create new member

--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { AppDataSource } from 'src/app.datasource';
 import { Member } from './entities/member.entity';
+import { CreateMemberDto } from './dto/createMember.dto';
 
 @Injectable()
 export class MemberService {
+  async create(createMemberDto: CreateMemberDto) {
+    const member = new Member();
+    member.name = createMemberDto.name;
+    member.email = createMemberDto.email;
+    member.tasks = undefined;
+
+    return await AppDataSource.createQueryBuilder().insert().into(Member).values(member).execute();
+  }
   async findAll(): Promise<Member[]> {
     const members = await AppDataSource.createQueryBuilder()
       .select('m')
